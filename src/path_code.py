c421cdbc650cecc25b1b05adde41ecb9cf113fb7
MohammadH Shirvani <127017807+MohammadH-Shirvani@users.noreply.github.com>
2024-11-15 17:46:12 +0330

    Update path_code.py

--- src/path_code.py
+++ src/path_code.py
@@ -1,25 +1,29 @@
 import airsim
 import time
+import sys
+import argparse
 import os
 import math
 
-# Camera Specific
-IMAGE_HEIGHT = 1280   # Same as the image height in setting.json
-IMAGE_WIDTH = 2560    # Same as the image width in setting.json
-CAMERA_FOV_HORIZONTAL = 90  # Horizontal field of view of the camera in degrees
+# Camera Specifics
+IMAGE_HEIGHT = 1280   # Same as the height in setting.json
+IMAGE_WIDTH = 2560    # Same as the width in setting.json
+CAMERA_FOV_HORIZONTAL = 90  # Horizontal field of view of the camera in degrees from setting.json
 
 # Vertical FoV Calculation
 aspect_ratio = IMAGE_WIDTH / IMAGE_HEIGHT
 horizontal_fov_rad = math.radians(CAMERA_FOV_HORIZONTAL)
-vertical_fov_rad = 2 * math.atan(math.tan(horizontal_fov_rad/2) / aspect_ratio)     # Vertical FoV
-CAMERA_FOV_VERTICAL = math.degrees(vertical_fov_rad)    # Vertical field of view
+vertical_fov_rad = 2 * math.atan(math.tan(horizontal_fov_rad/2) / aspect_ratio)
+CAMERA_FOV_VERTICAL = math.degrees(vertical_fov_rad)    # Vertical FoV
+
+# Region of interest defined as (x1, y1, x2, y2)
+roi = (IMAGE_WIDTH // 3, 0, IMAGE_WIDTH * 2 // 3, IMAGE_HEIGHT)
 
 # Overlap definition
-lateral_overlap = 0.8
-longtitudal_overlap = 0.9
+lateral_overlap = 0.6
+longtitudal_overlap = 0.8
 
-# Start number of image index
-index = 0
+index = 0   # First photo index
 
 class position:
     def __init__(self, pos):
@@ -29,7 +33,7 @@ class position:
 
 # Make the drone fly in a grid
 class DroneFlight:
-    def __init__(self, building_width = 26, initial_altitude=11, building_height=12):
+    def __init__(self, building_width = 26, initial_altitude=13, building_height=12):
         self.building_width = building_width
         self.initial_altitude = initial_altitude
         self.building_height = building_height
@@ -52,20 +56,20 @@ class DroneFlight:
 
     def fly_and_capture(self, altitude, direction):
         """
-        Fly the drone along the building's width at a given altitude and capture images with the specified horizontal overlap.
+        Fly the drone along the building's width at a given altitude and capture images with the specified overlap.
         """
         self.current_position = self.client.getMultirotorState().kinematics_estimated.position
         z = -altitude
         self.client.moveToPositionAsync(self.current_position.x_val, self.current_position.y_val, z, 2).join()
+        print(f'Current position = {self.current_position}\n Start position = {self.start} and \n Direction: {direction}')
         while abs(self.current_position.y_val - self.start.y_val) <= self.building_width:
+            
             # Capture image
             self.take_snapshot()
             # Move to next position
             self.current_position.y_val += direction * self.interval
-            print('Y val of Current position : ', self.current_position.y_val)
+            print('Y val of Current position after interval = ', self.current_position.y_val)
             self.client.moveToPositionAsync(self.current_position.x_val, self.current_position.y_val, z, 2).join()
-        
-        # Update start position of path after completing the flight along building width
         self.start = self.client.getMultirotorState().kinematics_estimated.position
 
     def perform_flights(self):
@@ -79,7 +83,7 @@ class DroneFlight:
         
         # get distance to wall
         self.distance_data = self.client.getDistanceSensorData(vehicle_name="MyDrone").distance
-        print("Initial Distance:", self.distance_data)
+        print("Distance:", self.distance_data)
 
         # Move to start position
         self.start = self.client.getMultirotorState().kinematics_estimated.position
@@ -94,14 +98,19 @@ class DroneFlight:
 
         self.current_altitude = self.initial_altitude
         vertical_coverage = self.calculate_vertical_coverage()
-        while self.current_altitude >= vertical_coverage/4:
-            # Fly from start to end
+        self.take_snapshot()
+        print('Current altitude : ', self.current_altitude)
+        while True:
+            # Fly from left to right
             self.fly_and_capture(self.current_altitude, 1)
             # Decrease the altitude
             self.current_altitude *= lateral_overlap
-            # Fly back from end to start
+            # Fly back in new decreased altitude
             self.fly_and_capture(self.current_altitude, -1)
             self.current_altitude *= lateral_overlap
+            if self.current_altitude >= vertical_coverage/2:
+                self.fly_and_capture(self.current_altitude, -1)
+                break
 
         # Disable API control and disarm
         print("descending")
@@ -137,10 +146,15 @@ class DroneFlight:
         print("Saved snapshot: {}".format(filename))
         return image_path
     
-
-
 if __name__ == "__main__":
 
-    flight = DroneFlight()
+    args = sys.argv
+    args.pop(0)
+    arg_parser = argparse.ArgumentParser("Orbit.py makes drone fly in a circle with camera pointed at the given center vector")
+    arg_parser.add_argument("--building_width", type=float, help="width of the building", default=26)
+    arg_parser.add_argument("--initial_altitude", type=float, help="initial altitude of flight (in positive meters)", default=13)
+    arg_parser.add_argument("--building_height", type=float, help="height of the building", default=12)
+    args = arg_parser.parse_args(args)    
+    flight = DroneFlight(args.building_width, args.initial_altitude, args.building_height)
     # Perform the flights
     flight.perform_flights()
